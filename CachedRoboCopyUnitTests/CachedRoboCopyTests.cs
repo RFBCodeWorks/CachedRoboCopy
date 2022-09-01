cd234c387498e6e8d87a73d5f5a9e01d7a1f3f90
RFBomb <robbery525@gmail.com>
2022-09-01 07:31:38 -0400

    Update CachedRoboCopyTests.cs

--- CachedRoboCopyUnitTests/CachedRoboCopyTests.cs
+++ CachedRoboCopyUnitTests/CachedRoboCopyTests.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RFBCodeWorks.CachedRoboCopy;
+using RoboSharp;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using CopyActionFlags = RoboSharp.CopyOptions.CopyActionFlags;
+using LoggingActionFlags = RoboSharp.LoggingOptions.LoggingActionFlags;
+using SelectionFlags = RoboSharp.SelectionOptions.SelectionFlags;
 namespace RFBCodeWorks.CachedRoboCopy.Tests
 {
     [TestClass()]
     public class CachedRoboCopyTests
     {
         [TestMethod()]
-        public void StartTest()
+        [DataRow(CopyActionFlags.Default, SelectionFlags.Default, LoggingActionFlags.Default, "Defaults")]
+        public void StartTest(CopyActionFlags copyAction, SelectionFlags selectionFlags, LoggingActionFlags loggingAction)
         {
-            var rc = TestPrep.GetRoboCommand(false, RoboSharp.CopyOptions.CopyActionFlags.Default, RoboSharp.SelectionOptions.SelectionFlags.Default);
+            var rc = TestPrep.GetRoboCommand(false, copyAction, selectionFlags, loggingAction);
             var crc = TestPrep.GetCachedRoboCopy(rc);
             rc.LoggingOptions.ListOnly = true;
             var results1 = TestPrep.RunTests(rc, crc, false).Result;
